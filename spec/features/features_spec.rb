a10b1c5253efc6eff684c8ae6f779aab788eec45
jtrtj <jt.r@live.com>
2018-07-09 12:16:44 -0600

    add routes for astronaut and fix test

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,17 +1,19 @@
 require 'rails_helper'
 describe 'user' do
   context 'visiting /astronauts' do
-    astro_1 = Astronaut.create(name: 'sgs', age: 23, job: 'sadg')
-    astro_2 = Astronaut.create(name: 'gfd', age: 23, job: 'hgfd')
+    it 'sees a list of astronauts and their attributes' do
+      astro_1 = Astronaut.create(name: 'sgs', age: 23, job: 'sadg')
+      astro_2 = Astronaut.create(name: 'gfd', age: 23, job: 'hgfd')
 
-    visit astronauts_path
+      visit astronauts_path
 
-    expect(page).to have_content(astro_1.name)
-    expect(page).to have_content(astro_1.age)
-    expect(page).to have_content(astro_1.job)
-    expect(page).to have_content(astro_2.name)
-    expect(page).to have_content(astro_2.age)
-    expect(page).to have_content(astro_2.job)
+      expect(page).to have_content(astro_1.name)
+      expect(page).to have_content(astro_1.age)
+      expect(page).to have_content(astro_1.job)
+      expect(page).to have_content(astro_2.name)
+      expect(page).to have_content(astro_2.age)
+      expect(page).to have_content(astro_2.job)
+    end
   end
 end
 =begin
