6248d0253244ac767b7a59a13751cb63ebd51229
jtrtj <jt.r@live.com>
2018-07-09 12:14:55 -0600

    write first test and migrate

---
+++ spec/features/features_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+describe 'user' do
+  context 'visiting /astronauts' do
+    astro_1 = Astronaut.create(name: 'sgs', age: 23, job: 'sadg')
+    astro_2 = Astronaut.create(name: 'gfd', age: 23, job: 'hgfd')
+
+    visit astronauts_path
+
+    expect(page).to have_content(astro_1.name)
+    expect(page).to have_content(astro_1.age)
+    expect(page).to have_content(astro_1.job)
+    expect(page).to have_content(astro_2.name)
+    expect(page).to have_content(astro_2.age)
+    expect(page).to have_content(astro_2.job)
+  end
+end
+=begin
+As a visitor,
+When I visit '/astronauts'
+I see a list of astronauts with the following info:
+ - Name
+ - Age
+ - Job
+ (e.g. "Name: Neil Armstrong" Age: 37 Job: Commander")
+=end
